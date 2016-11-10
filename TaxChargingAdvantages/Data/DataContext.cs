b24cc97f840e61803bf4b1ec058a030946e90a8d
eduardosmatheus <mathe@DESKTOP-9HER3CT>
2016-11-10 21:34:11 -0200

    Initializing Entity Framework and W.P.F. This application might persist data and return it in a desktop client.

---
+++ TaxChargingAdvantages/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaxChargingAdvantages.Data
+{
+    class DataContext : DbContext
+    {
+        public DataContext() 
+			: base("TaxConnectionString")
+        {
+
+        }
+    }
+}
